b0b5f3e9b27bb06f4f90a5e3b9b669b451f59831
Sujal <sujal.kumar311@gmail.com>
2023-04-15 00:40:30 +0530

    Install reactjs and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-green-400 text-center underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
