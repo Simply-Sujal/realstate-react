aeaa217cc69b7c41e0a1d15388135b5ffec9e4dd
Sujal <sujal.kumar311@gmail.com>
2023-06-26 16:45:47 +0530

    Completed authentication part using firebase

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import OAuth from '../Components/OAuth';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState("");
@@ -8,6 +10,18 @@ const ForgotPassword = () => {
     function onChange(e) {
         setEmail(e.target.value);
     }
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        try {
+            const auth = getAuth();
+            await sendPasswordResetEmail(auth, email)
+            toast.success("Email was sent")
+        } catch (error) {
+            toast.error("could not send reset password")
+        }
+    }
+
     return (
         <div>
             <h1 className='text-3xl text-center mt-6 font-bold'>Forgot Password</h1>
@@ -16,7 +30,7 @@ const ForgotPassword = () => {
                     <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="Key" className='w-full rounded-2xl' />
                 </div>
                 <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-                    <form >
+                    <form onSubmit={onSubmit}>
                         <input type="email" id='email' value={email} onChange={onChange} placeholder='Email address' className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' />
 
 
